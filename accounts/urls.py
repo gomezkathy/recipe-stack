1d5c459621e4de12ae924685d80a335967607dcc
kathy <gomezkathy1997@gmail.com>
2023-10-18 18:44:11 -0700

    login + signup css

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,5 @@ from accounts.views import signup, user_login
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
-    path('recipes/', include('recipes.urls')),
     path('login/', user_login, name='user_login'),
 ]
